ccdea556d113549a18a379228014135807b8c8a8
eshonqul6772 <abdulazizoveshonqul66@gmail.com>
2025-03-13 14:19:44 +0500

    deployment in heroku

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,34 +1,45 @@
 import * as process from 'process';
+import { config as dotenvConfig } from 'dotenv';
 import 'dotenv/config';
 import { join } from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
 
+const envFilePath = process.env['APP_STATUS'] === 'prod' ? '.env.production' : '.env.development';
+dotenvConfig({ path: envFilePath });
+
 export const configuration = {
-  isProd: process.env['APP_STATUS'] == 'prod',
-  port: process.env['PORT'] || 3000,
+  isProd: process.env['APP_STATUS'] === 'prod',
+  port: process.env['PORT'] || 4445,
   getDataSourceConfig(): DataSourceOptions {
     return {
       type: 'postgres' as any,
-      host: 'localhost',
-      port: 5432,
-      username: 'makeup',
-      password: '6772',
-      database: 'makeup',
+      host:
+        process.env['DB_HOST'] || 'cd27da2sn4hj7h.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+      port: parseInt(process.env['DB_PORT']) || 5432,
+      username: process.env['DB_USERNAME'] || 'u4gudisuseso0i',
+      password:
+        `${process.env['DB_PASSWORD']}` ||
+        'p06ab451753fc047f475c8dcbda4f654f5d8dc85231f5582a15f887329a320c92',
+      database: process.env['DB_DATABASE'] || 'd5olqocfprquot',
       entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
       synchronize: true,
     };
   },
+
   getTypeOrmConfig(): TypeOrmModuleOptions {
     const ormConfig = {
-      type: 'postgres' as any,
-      host: 'localhost',
-      port: 5432,
-      username: 'makeup',
-      password: '6772',
-      database: 'makeup',
+      type: (process.env['DB_TYPE'] as any) || 'postgres',
+      host:
+        process.env['DB_HOST'] || 'cd27da2sn4hj7h.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+      port: parseInt(process.env['DB_PORT']) || 5432,
+      username: process.env['DB_USERNAME'] || 'u4gudisuseso0i',
+      password:
+        `${process.env['DB_PASSWORD']}` ||
+        'p06ab451753fc047f475c8dcbda4f654f5d8dc85231f5582a15f887329a320c92',
+      database: process.env['DB_DATABASE'] || 'd5olqocfprquot',
       entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
-      synchronize: true,
+      synchronize: false,
       logging: false,
     };
 
